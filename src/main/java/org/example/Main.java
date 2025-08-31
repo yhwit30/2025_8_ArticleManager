8174af8ef2f058ddcd437971d51ce155b5d778c4
yhwit30 <yhwit30@gmail.com>
2025-08-31 12:37:21 +0900

    회원등록, 회원목록보기(개발자용) 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     static List<Article> articleList = new ArrayList<>();
+    static List<Member> memberList = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -17,6 +18,7 @@ public class Main {
         System.out.println("== 프로그램 시작 ==");
 
         int lastId = 3;
+        int lastMemberId = 0;
 
 
         while (true) {
@@ -31,7 +33,31 @@ public class Main {
             if (cmd.equals("exit")) {
                 break;
             }
-            if (cmd.equals("article write")) {
+            if (cmd.equals("member list")) {
+                for (Member member : memberList) {
+                    System.out.println("member" + member.toString());
+                }
+            } else if (cmd.equals("member join")) {
+                System.out.println("==회원가입==");
+                int id = lastMemberId + 1;
+                System.out.print("아이디 : ");
+                String loginId = sc.nextLine().trim();
+                System.out.print("비밀번호 : ");
+                String loginPw = sc.nextLine().trim();
+                System.out.print("이름 : ");
+                String name = sc.nextLine().trim();
+                String regDate = Util.getNowStr();
+                String updateDate = Util.getNowStr();
+
+                Member member = new Member(id, loginId, loginPw, name, regDate, updateDate);
+
+                memberList.add(member);
+
+                System.out.println(id + "번 회원이 등록되었습니다.(개발자용)");
+                System.out.println(name + "회원님 등록을 환영합니다.");
+
+                lastMemberId++;
+            } else if (cmd.equals("article write")) {
                 System.out.println("==게시글 작성==");
                 int id = lastId + 1;
                 System.out.print("제목 : ");
@@ -56,15 +82,15 @@ public class Main {
                 String searchKeyword = cmd.substring("article list".length()).trim();
 
                 List<Article> forPrintArticles = articleList;
-                if(searchKeyword.length() > 0){
+                if (searchKeyword.length() > 0) {
                     forPrintArticles = new ArrayList<>();
                     System.out.println("검색어 : " + searchKeyword);
-                    for(Article article : articleList){
-                        if(article.getTitle().contains(searchKeyword)){
+                    for (Article article : articleList) {
+                        if (article.getTitle().contains(searchKeyword)) {
                             forPrintArticles.add(article);
                         }
                     }
-                    if(forPrintArticles.size() == 0){
+                    if (forPrintArticles.size() == 0) {
                         System.out.println("검색결과 없음");
                         continue;
                     }
@@ -145,6 +171,84 @@ public class Main {
         articleList.add(new Article(1, "제목1", "내용1", "2025-8-20 12:12:12", "2025-9-1 12:30:30"));
         articleList.add(new Article(2, "제목2", "내용2", "2025-8-26 12:23:12", Util.getNowStr()));
         articleList.add(new Article(3, "제목3", "내용3", "2025-8-27 12:12:44", Util.getNowStr()));
+    }
+}
+
+class Member {
+    private int id;
+    private String loginId;
+    private String loginPw;
+    private String name;
+    private String regDate;
+    private String updateDate;
+
+    public Member(int id, String loginId, String loginPw, String name, String regDate, String updateDate) {
+        this.id = id;
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.name = name;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", loginId='" + loginId + '\'' +
+                ", loginPw='" + loginPw + '\'' +
+                ", name='" + name + '\'' +
+                ", regDate='" + regDate + '\'' +
+                ", updateDate='" + updateDate + '\'' +
+                '}';
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getLoginPw() {
+        return loginPw;
+    }
+
+    public void setLoginPw(String loginPw) {
+        this.loginPw = loginPw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
